48f7c02ef0224cae8ea9baea1bdc3c5f70687786
Ali Parvizi <snparvizi75@gmail.com>
2019-08-14 10:36:43 +0430

    int with commas

--- account/admin.py
+++ account/admin.py
@@ -1,10 +1,28 @@
 from django.contrib import admin
-from .models import  *
+from .models import *
+import locale
+
+
 # Register your models here.
+
+def int_with_commas(x):
+    if type(x) not in [type(0), ]:
+        raise TypeError("Parameter must be an integer.")
+    if x < 0:
+        return '-' + int_with_commas(-x)
+    result = ''
+    while x >= 1000:
+        x, r = divmod(x, 1000)
+        result = ",%03d%s" % (r, result)
+    return "%d%s" % (x, result)
 
 
 class AccountAdmin(admin.ModelAdmin):
     list_display = ['name', 'balance']
+
+    @staticmethod
+    def balance(instance):
+        return int_with_commas(instance.balance)
 
 
 class TransactionAdmin(admin.ModelAdmin):
@@ -17,4 +35,3 @@ class TransactionAdmin(admin.ModelAdmin):
 
 admin.site.register(Account, AccountAdmin)
 admin.site.register(Transaction, TransactionAdmin)
-
