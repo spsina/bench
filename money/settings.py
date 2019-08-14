bc85dc99fcd8475f15adf20d2646b243b1756b7a
Ali Parvizi <snparvizi75@gmail.com>
2019-08-14 10:18:01 +0430

    .idea | postgres

--- money/settings.py
+++ money/settings.py
@@ -76,13 +76,23 @@ WSGI_APPLICATION = 'money.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'bench',
+        'USER': 'sina',
+        'PASSWORD': 'thecode',
+        'HOST': '',
+        'PORT': '',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
